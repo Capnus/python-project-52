c7deafca5be6ad3f13f9759683150e2b0f1cd975
capn <kehsa754@gmail.com>
2025-04-24 12:43:11 +0000

    temp

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import IndexView, UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='home'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('users/', include('task_manager.users.urls')),
 ]
